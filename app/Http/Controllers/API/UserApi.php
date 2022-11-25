0ff157ace158d73dd1ef995fd36d32e64b932d9a
jfmartineb <69642154+jfmartineb@users.noreply.github.com>
2022-11-24 22:55:05 -0500

    added Apis

--- app/Http/Controllers/API/UserApi.php
+++ app/Http/Controllers/API/UserApi.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Terapia;
 use App\Http\Resources\UserCollection;
 use Illuminate\Http\Request;
 
@@ -13,9 +14,23 @@ class UserApi extends Controller
     {   
         $dataApi = [];
         $id = $request->input('id');
-        $usuario = User::findOrFail((int)$id);
+        $usuario = User::where('document', '=', $id)->find(1);
         $dataApi['data'] = json_encode($usuario);
         $dataApi['additionalData'] = json_encode([ 'empresa' => 'EAFIT', 'linkEmpresa' => 'http://127.0.0.1:8000/paciente']);;
+        return json_encode($dataApi);
+    }
+
+    public function terapias(Request $request)
+    {
+        $dataApi = [];
+        $id = $request->input('id');
+        $usuario = User::findOrFail($id);
+        $terapias = [];
+        foreach ($usuario->getTerapias() as $terapia){
+            $terapias[$terapia->getId()] = $terapia->getEjercicio();
+        }
+        $dataApi['data'] = json_encode($terapias);
+        $dataApi['additionalData'] = json_encode([ 'empresa' => 'EAFIT', 'linkEmpresa' => 'http://127.0.0.1:8000/paciente']);
         return json_encode($dataApi);
     }
 }
